9130f84a2c119a6fcc5082af8b8017f120e6d0f8
Abel Ponce <vnponce8@gmail.com>
2021-01-10 23:45:11 -0600

    removing iportin from styling provider

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
 
 import SignIn from "./components/Signin";
 import Signup from "./components/Signup";
